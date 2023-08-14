ae5d6cf7b925468b35a98540ec6baad09e919dc2
Auridebson Sousa <auridebson@vutus.com>
2023-08-14 19:05:26 -0300

    Subindo exercicios

---
+++ script.js
@@ -0,0 +1,20 @@
+const exercicios = {
+
+    input_dados: function() {
+        const cp_nome = document.querySelector('#cp-tex-nome')
+        const cp_sobrenome = document.querySelector('#cp-tex-sobrenome')
+        const cp_dat_nascimento = document.querySelector('#cp-dat-nascimento')
+
+        const texResultado = `Nome: ${cp_nome} <br>Sobrenome: ${cp_sobrenome}<br>${cp_dat_nascimento}`
+
+        container-resultado
+        
+    }
+
+
+
+
+
+
+
+}

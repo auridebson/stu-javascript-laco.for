3e8636e5f38192db8b21b0f36545276c1640390e
Auridebson Lima <auridebson@vutus.com>
2023-10-22 13:22:10 -0300

    Update

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-const exercicios = {
+function contagem() {
+    let res1 = document.querySelector('#resContagem1')
+    let res2 = document.querySelector('#resContagem2')
+    let txarea = document.querySelector('#txArea')    
 
-    input_dados: function() {
-        const cp_nome = document.querySelector('#cp-tex-nome')
-        const cp_sobrenome = document.querySelector('#cp-tex-sobrenome')
-        const cp_dat_nascimento = document.querySelector('#cp-dat-nascimento')
+    let numInicio = document.querySelector('#txNumInicio')
+    let numFim = document.querySelector('#txNumFim')
+    let numPasso = document.querySelector('#txNumPasso')
+    let emogi = '1F600'
 
-        const texResultado = `Nome: ${cp_nome} <br>Sobrenome: ${cp_sobrenome}<br>${cp_dat_nascimento}`
-
-        container-resultado
+    if (numInicio.value.length == 0 || numFim.value.length == 0 || numPasso.value <= 0) {
+        alert('ERRO: Defina os valores para prosseguir')
+    } else {
+        res1.innerHTML = 'Contando:<br>'
+        let i = Number(numInicio.value)
+        let f = Number(numFim.value)
+        let p = Number(numPasso.value)
         
+        if (i>f) {
+            for (let c = i; c >= f; c -= p) {
+                res1.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for (let c = i; c <= f; c += p) {
+                res1.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res1.innerHTML += '\u{1F3C1}'
+
     }
+}
 
-
-
-
-
+function tabuada() {
+    let num = document.querySelector('#txNumero')
+    let tab = document.querySelector('#txSelect')
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = ''
+    if (num.value > 0) {
+        for (let i = c; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+        
+    } else {
+        window.alert('ERRO: Digite um valor válido para a Tabuada')
+    }
 
 
 }
